53bffff984da3afb8a30e25ba0a83f4554a0179f
William Song <30965609+Freakwill@users.noreply.github.com>
2026-01-20 21:56:43 +0800

    Add files via upload

---
+++ ollama_ext/mixin.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python3
+
+import shlex
+from .commands import Commands
+
+MAX_LEN = 1000
+
+
+class ChatMixin:
+
+    @property
+    def history(self):
+        return self._history
+
+    @property
+    def description(self):
+        return self._description
+
+    @description.setter
+    def description(self, v):
+        self._description = v
+        if bool(self.history) and self.history[0]["role"] == "system":
+            self.history[0] = {"role": "system", "content": v}
+        else:
+            self.history.insert(0, {"role": "system", "content": v})
+
+    def init(self):
+        print(f'💻System: The chat has started. Agent `{self.name.capitalize()}` will serve you.')
+
+    def run(self, description=None):
+        # To chat with AI
+
+        self.init()
+
+        while True:
+            user_input = input("👨User: ")
+            if user_input.strip().lower() in {'exit', 'quit', 'bye'}:
+                print(f"🤖{self.name.capitalize()}: Bye.")
+                break
+            self.reply(user_input)
+            self.post_process()
+
+    def post_process(self):
+        max_len = 20
+        while len(self.history) > max_len:
+            self.history.pop(1)
+
+    def demo(self, prompts):
+        self.init()
+        for p in prompts:
+            print("👨User:", p)
+            self.reply(p)
+
+    def reply(self, user_input, messages=[], memory_flag=True, max_retries=10):
+        """The reply of the AI chat assistant
+        
+        Args:
+            user_input (str): The query inputed by the user
+            messages (list, optional): Additional information before user input
+            memory_flag (bool, optional): save the messages
+            max_retries (int, optional): The maximum of retries
+        """
+
+        if user_input.startswith(':'):
+            a, v = user_input[1:].split()
+            self.chat_params[a] = convert(v)
+            print(f'💻System: The parameter `{a}` of chat method is set to be `{v}`.')
+        elif user_input.startswith('#'):
+            a, v = user_input[1:].split()
+            setattr(self, a, v)
+            print(f'💻System: The attribute `{a}` of chat object is set to be `{v}`.')
+        elif user_input.startswith('!'):
+            cmd = user_input.lstrip('! ')
+            cmd, *args = shlex.split(cmd)
+            try:
+                getattr(Commands, cmd)(self, *args)
+            except AttributeError:
+                print(f"💻System: {cmd} is not registered yet!")
+            except Exception as e:
+                print(f"💻System: The execution of {cmd} raise an error: {e}!")
+        else:
+            message = {"role": "user", "content": user_input}
+            messages.append(message)
+            print("🤖" + self.name.capitalize(), end=": ")
+            assistant_reply = self._reply(self.history + messages)
+            print(assistant_reply)
+
+            if memory_flag:
+                messages.append({"role": "assistant", "content": assistant_reply})
+                self.history.extend(messages)
+
+    @property
+    def history_size(self):
+        return sum(len(d["content"]) for d in self.history)
+
+    
---
+++ ollama_ext/ollama.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python3
+
+from .mixin import ChatMixin
+import ollama
+from ollama import Client, ResponseError
+
+from dotenv import dotenv_values
+config = dotenv_values(".env")
+api_key = config.get('OLLAMA_API_KEY', '')
+
+
+class ModelNotFoundError(Exception):
+    """
+    Do not find the model locally
+    """
+    def __init__(self, model):
+        self.model = model
+        super().__init__(f"Model `{self.model}` is not found in locally!")
+
+
+class OllamaChat(ChatMixin, Client):
+    # see https://github.com/ollama/ollama-python
+
+    get_reply = lambda response: response.message.content
+
+    def __init__(self, description='You are a very intelligent agent', history=[], name='Assistant', model='gpt-oss:120b', api_key=api_key, *args, **kwargs):
+        if ':' not in model:
+            model += ':latest'
+        if api_key:
+            super().__init__(host='https://ollama.com',
+                headers={'Authorization': 'Bearer ' + api_key}, *args, **kwargs)
+        else:
+            super().__init__(model=model, *args, **kwargs)
+            if any(self.model==m.model for m in ollama.list().models):
+                raise ModelNotFoundError(self.model)
+        self._history = history
+        self.description = description
+        self.name = name
+        self.model = model
+        self.chat_params = {}
+
+    def _reply(self, messages, max_retries=10):
+        """Wrapper of `chat.completions.create` method of LLM
+        The reply method of the AI chat assistant
+        as a mapping message --> response
+
+        Args:
+            message: the prompt object inputed by the user
+            max_retries (int, optional): the number of times to get response
+        """
+
+        try:
+            response = self.chat(
+                model=self.model,
+                messages = messages,
+                **self.chat_params)
+            return response.message.content
+        except Exception as e:
+            print(f"💻System: An error occurred after {max_retries} attempts: {e}")
+
+    def __enter__(self, *args, **kwargs):
+        import sh
+        sh.brew.services.start.ollama()
+        return self
+
+    def __exit__(self, *args, **kwargs):
+        import sh
+        sh.brew.services.stop.ollama()
+
+
+class LocalOllamaChat(OllamaChat):
+
+    def __init__(self, model='gemma3', *args, **kwargs):
+        super().__init__(model=model, api_key=None, *args, **kwargs)
+    
+    def init(self, *args, **kwargs):        
+        if any(self.model==m.model for m in ollama.list().models):
+            raise ModelNotFoundError(self.model)
+        super().init(*args, **kwargs)
+
